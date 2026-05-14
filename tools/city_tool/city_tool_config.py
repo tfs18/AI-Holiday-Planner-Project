99882a3a012f08fb252829503772c8a0e67afda3
maxshen01 <maxshen2503@gmail.com>
2026-05-14 15:50:38 +0100

    improve modularity and coding practices of city_tool

---
+++ tools/city_tool/city_tool_config.py
@@ -0,0 +1,5 @@
+DEFAULT_COUNTRY_LIMIT = 10
+CITY_TYPE = "CITY"
+SORTING_METHOD = "-population"
+API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+X_RAPIDAPI_HOST = "wft-geo-db.p.rapidapi.com"
