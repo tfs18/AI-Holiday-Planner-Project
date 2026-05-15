b6f69104ede3d6b9bdd4a7ec48704c750873d35b
maxshen01 <maxshen2503@gmail.com>
2026-05-15 17:08:30 +0100

    add logging setup to system

--- agent.py
+++ agent.py
@@ -1,17 +1,22 @@
 from google import genai
 from dotenv import load_dotenv
 import os
+from logs.logging_config import setup_logging
+import logging
 
 load_dotenv()
 
+setup_logging()
+
+#This is how to get the right logging instance.
+logger = logging.getLogger("holiday-agent")
+
+logger.info("App started")
+
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-# client = genai.Client()
+client = genai.Client()
 
-# response = client.models.generate_content(
-#     model="gemini-3-flash-preview", contents="explain in a few words what the optimal diet is?"
-# )
-# print(response.text)
-
-from tools.city.city_tool import get_top_cities
-
-print(get_top_cities("FR"))
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", contents="explain in a few words what the optimal diet is?"
+)
+print(response.text)
