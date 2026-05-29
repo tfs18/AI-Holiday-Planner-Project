2900fc849bc2f7632e288cde3006d4288fbc4ad0
tfs18 <tfitzsmith1@gmail.com>
2026-05-29 16:55:49 +0100

    modified to test scoring function

--- test_tools.py
+++ test_tools.py
@@ -1,7 +1,15 @@
+# from logs.logging_config import setup_logging
+# setup_logging()
+
+# from tools.weather.weather_tool import get_forecasts_for_country, pretty_print_forecasts
+
+# result = get_forecasts_for_country("GB")
+# pretty_print_forecasts(result)
+
 from logs.logging_config import setup_logging
+from tools.scoring.scoring_tool import rank_days_for_country, pretty_print_rankings
+
 setup_logging()
 
-from tools.weather.weather_tool import get_forecasts_for_country, pretty_print_forecasts
-
-result = get_forecasts_for_country("GB")
-pretty_print_forecasts(result)
+result = rank_days_for_country("GB", "warm")
+pretty_print_rankings(result)
